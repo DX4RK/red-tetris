f054bb3f4843de2c892f5d43521580406400451c
nolan <firesocietygames@gmail.com>
2026-06-30 17:57:00 +0200

    added server structure

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,18 +1,40 @@
-// Import the framework and instantiate it
 import Fastify from 'fastify'
+import fastifyEnv from '@fastify/env'
+import fastifySocketIO from 'fastify-socket.io'
+
+import loggerConfig from './config/logger.json' with { type: 'json' }
+
 const fastify = Fastify({
-  logger: true
+  logger: loggerConfig,
 })
 
-// Declare a route
-fastify.get('/', async function handler (request, reply) {
-  return { hello: 'world' }
-})
+const schema = {
+  type: 'object',
+  required: ['PORT'],
+  properties: {
+    PORT: {
+      type: 'string',
+      default: '3000',
+    },
+  },
+}
 
-// Run the server!
-try {
-  await fastify.listen({ port: 3000 })
-} catch (err) {
+const start = async () => {
+  await fastify.register(fastifyEnv, {
+    schema,
+    dotenv: true,
+  })
+
+  fastify.register(fastifySocketIO as any, {})
+  fastify.get('/', async () => {
+    return { hello: 'world' }
+  })
+
+  const port = Number(fastify.config.PORT)
+  await fastify.listen({ port })
+}
+
+start().catch((err) => {
   fastify.log.error(err)
   process.exit(1)
-}
+})
