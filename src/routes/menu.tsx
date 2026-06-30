e833b152aab785df8247df7e5edd595c99cddb38
nolan <firesocietygames@gmail.com>
2026-06-30 16:29:17 +0200

    added menu page

---
+++ src/routes/menu.tsx
@@ -0,0 +1,10 @@
+import { Label } from "@/components/ui/label"
+
+export function Menu() {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+		<Label className="text-7xl font-bold drop-shadow-lg drop-shadow-red-500/50">Menu</Label>
+    </div>
+
+  )
+}
